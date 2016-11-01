e44fbaafa89a099ca1f2be8dc02477e1cfa12464
Raghu Betina <raghu@firstdraft.com>
2016-11-01 22:04:04 +0000

    Generate actions resource with starter:resource

---
+++ app/controllers/actions_controller.rb
@@ -0,0 +1,46 @@
+class ActionsController < ApplicationController
+  def index
+    @actions = Action.all
+  end
+
+  def show
+    @action = Action.find(params[:id])
+  end
+
+  def new
+    @action = Action.new
+  end
+
+  def create
+    @action = Action.new
+
+    if @action.save
+      redirect_to "/actions", :notice => "Action created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @action = Action.find(params[:id])
+  end
+
+  def update
+    @action = Action.find(params[:id])
+
+
+    if @action.save
+      redirect_to "/actions", :notice => "Action updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @action = Action.find(params[:id])
+
+    @action.destroy
+
+    redirect_to "/actions", :notice => "Action deleted."
+  end
+end
