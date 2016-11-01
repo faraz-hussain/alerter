c0bdaa1305c521372502cd048212fdd91e7d01f7
Raghu Betina <raghu@firstdraft.com>
2016-11-01 22:04:07 +0000

    Direct association between actions and requests

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,6 +1,9 @@
 class Action < ApplicationRecord
   # Direct associations
 
+  has_one    :request,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,8 @@
 class Request < ApplicationRecord
   # Direct associations
 
+  belongs_to :action
+
   belongs_to :requester
 
   belongs_to :keyword
